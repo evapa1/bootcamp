5c5be5323d7d2e4d2fb5ad7eacc4edcbab9d4787
Eva <89265028+evapa1@users.noreply.github.com>
2022-11-08 00:18:45 -0500

    Create CardEditor.js

---
+++ src/CardEditor.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CardEditor.css';
+
+class CardEditor extends React.Component{
+  //component keeping own state
+  render() {
+    return(
+      <div>
+        <h2>Card Editor</h2>
+      </div>
+     );
+  };
+}
+
+export default CardEdtitor;
+    
