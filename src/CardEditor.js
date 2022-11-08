beeb321c9be3f99666ff4f42154b6f688f8e16d5
Eva <89265028+evapa1@users.noreply.github.com>
2022-11-08 15:51:07 -0500

    Update CardEditor.js

--- src/CardEditor.js
+++ src/CardEditor.js
@@ -3,10 +3,37 @@ import './CardEditor.css';
 
 class CardEditor extends React.Component{
   //component keeping own state
+  // render in a table
+  //mpa function with any array, can map over that array of objects
+  // takesevery element in cards array and map it to jsx element
   render() {
+    const cards = this.props.cards.map() => {
+      //takes in current card and return elements want card to become
+       //iterating over each card and mapping it to a fake row
+      return {
+         <tr>
+            <td>card front</td>
+            <td>card back</td>
+            <td>
+              <button>Delete card</button>
+            </td>
+          </tr>
+        };
+    });
+    
     return(
       <div>
         <h2>Card Editor</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Front</th>
+              <th>Back</th>
+              <th>Delete</th>
+            </tr>
+          </thead> 
+          <tbody>{cards}</tbody>
+         </table>
       </div>
      );
   };
