87525e6ce90c8a8a24fdb1d0287cc60834d6d896
Eva <89265028+evapa1@users.noreply.github.com>
2022-11-08 15:45:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import CardEditor from './CardEditor';
 
-function App() {
-  return <CardEditor />
+class App extends React.Component { //way to create state
+  constructor(props) {
+    super(props);
+     this.state = {
+      cards: [
+        { front: 'front1', back: 'back1' },
+        { front: 'front1', back: 'back1' },
+      ];
+     };
+  }
+  
+  render() {
+    return <CardEditor cards={this.state.cards] />;
+  }
 }
 
 export default App;
